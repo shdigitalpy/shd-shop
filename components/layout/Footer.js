90090498cfcce8cc02809b3de83bb0aa38a21b3d
Sandro Huber <sandro@sh-digital.ch>
2022-05-30 13:34:50 +0200

    footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -292,7 +292,7 @@ const Footer = () => {
                         <div className="col-xl-4 col-lg-6 col-md-6">
                             <p className="font-sm mb-0">
                                 &copy; {(new Date().getFullYear())},{" "}
-                                <strong className="text-brand">SH Digital</strong> <br />
+                                <strong className="text-brand">Bio Hundeshop</strong> <br />
                                 Alle Rechte vorbehalten
                             </p>
                         </div>
